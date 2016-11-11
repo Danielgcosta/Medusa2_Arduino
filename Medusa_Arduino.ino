8902de20e3b00622a300afb4f854e8f0031f2dfc
Daniel Guimarães Costa <friendschampionship@gmail.com>
2016-11-11 05:07:38 -0200

    First commit for C++ to Arduino factoring

--- Medusa_Arduino.ino
+++ Medusa_Arduino.ino
@@ -1,13 +1,24 @@
+/*
+Medusa 2.0	Robô flutuante de acompanhamento ambiental
+Pontifícia Universidade Católica do Rio de Janeiro
+Departamento de Engenharia Elétrica
+
+Name:		Medusa2.ino
+Created:	2016
+Author:	Daniel Guimarães Costa
+Contact:	danielgc@ele.puc-rio.br
+
+Orientadores:
+Marley Vellasco	marley@ele.puc-rio.br
+Karla Figueiredo	karla@ele.puc-rio.br
+*/
+
 void setup()
 {
-
-  /* add setup code here */
-
+	Serial.begin(9600);
 }
 
 void loop()
 {
-
-  /* add main program code here */
-
+	Serial.println("It's working. It's workiiiing!!!");
 }
